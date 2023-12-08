f4d2ebc22492862303c79ba29ddd02973b95356b
Jerome Schaeffer <jer.schaeffer@gmail.com>
2023-12-08 18:51:31 +0900

    day 08

---
+++ day_08/index.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf8");
+const lines = input.split("\n");
+const moves = lines[0].split("");
+const lastMoves = [];
+
+const nextMove = (i = 0) => {
+    const lastMove = lastMoves[i] ?? 0;
+    const move = moves[lastMove] ?? moves[lastMove % moves.length];
+    lastMoves[i] = lastMove + 1;
+
+    return move;
+};
+
+/**
+ *
+ * @param {string} lines
+ * @returns {Map<string, string>}
+ */
+const extractMap = lines => {
+    const map = new Map();
+
+    lines.forEach(line => {
+        line = line.replace(/\s|\(|\)/g, "");
+        const [key, paths] = line.split("=");
+        map.set(key, paths.split(","));
+    });
+
+    return map;
+};
+
+const map = extractMap(lines.slice(2));
+
+// star 1
+let node = "AAA";
+let star1 = 0;
+while (node !== "ZZZ") {
+    const next = map.get(node);
+    node = nextMove() === "L" ? next[0] : next[1];
+    star1++;
+}
+
+// star 2
+// DISCLAIMER GCD, LCM and LCMM are grabbed from the internet
+
+/**
+ * Least common multiple (LCM) is the smallest number that is divisible by both a and b.
+ * LCM = (a * b) / GCD(a, b)
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+const lcm = (a, b) => {
+    return (a * b) / gcd(a, b);
+};
+
+/**
+ * Least common multiple of an array of numbers.
+ * @param {number[]} nums
+ * @returns {number}
+ */
+const lcmm = nums => {
+    let res = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        res = lcm(res, nums[i]);
+    }
+    return res;
+};
+
+/**
+ * GCD (Greatest Common Divisor) is the largest number that divides both of the numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} GCD (Greatest Common Divisor)
+ */
+const gcd = (a, b) => {
+    // Euclidean algorithm
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+};
+
+let nodes = [...map.keys()].filter(node => node.endsWith("A"));
+let periods = nodes.map(_ => 0);
+let count = 0;
+while (!periods.every(n => n !== 0)) {
+    nodes = nodes.map((node, i) => {
+        const next = map.get(node);
+        return nextMove(i) === "L" ? next[0] : next[1];
+    });
+    nodes.forEach((node, i) => {
+        if (node.endsWith("Z") && periods[i] === 0) {
+            periods[i] = count;
+        }
+    });
+    count++;
+}
+periods = periods.map(n => n + 1);
+const star2 = lcmm(periods);
+
+console.log("star 1: ", star1);
+console.log("star 2: ", star2);
